fca8e85aac289b40eb060d526150bc519c290a3f
kunal <kunalshukla7999@gmail.com>
2026-03-11 09:49:57 +0530

    Change printed name to 'kunal' in UC2OOPS

--- src/UC2OOPS.java
+++ src/UC2OOPS.java
@@ -1,6 +1,6 @@
 public class UC2OOPS {
     public static void main(String[] args) {
         String name = args[0];
-        System.out.println("Hello " + name);
+        System.out.println("Hello " + kunal);
     }
 }
